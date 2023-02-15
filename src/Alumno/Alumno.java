b08087bd411973864bc6277c78f7efe7511e11a2
jiada <jiada@JD>
2023-02-15 13:24:48 +0100

    Estado final del ejercicio

--- src/Alumno/Alumno.java
+++ src/Alumno/Alumno.java
@@ -1,13 +1,43 @@
 package Alumno;
 
 /**
- * Alumno.java
- * Definición de la clase Alumno
- * @author Luis José Sánchez
+ * Alumno.java Definición de la clase Alumno
+ *
+ * @author Jiada Chen
  */
-
 public class Alumno {
-  public String nombre;
-  public double notaMedia = 0.0;
-  
+
+    private String nombre;
+    private double notaMedia;
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the notaMedia
+     */
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+
+    /**
+     * @param notaMedia the notaMedia to set
+     */
+    public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+    
+    
+
 }
--- src/alumnos/Alumnos.java
+++ src/alumnos/Alumnos.java
@@ -1,50 +1,62 @@
 package alumnos;
+
 import Alumno.Alumno;
 import java.util.Scanner;
 
+/**
+ * Clase Alumno
+ * @author Jiada Chen
+ */
 public class Alumnos {
-  public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-    
-    // Define la estructura, un array de 5 alumnos
-    // pero no crea los objetos
-    Alumno[] alum = new Alumno[5];
-    
+    /**
+     * Define la estructura, un array de 5 alumnos pero no crea los objetos
+     * @param args argoritmo
+     */
+    public static void main(String[] args) {
+        Alumno[] alum = new Alumno[5];
+        introducirDatoAlumno(alum);
+        mostrarInformacion(alum);
+    }
 
-    // Pide los datos de los alumnos /////////////////////////////////
-        
-    System.out.println("A continuacion debera introducir el nombre y la nota media de 5 alumnos.");
+    /**
+     * Se pide la informacion del alumno como nombre y su nota
+     * @param alum Array donde se almacena los alumnos
+     * @throws java.util.InputMismatchException No ser un número
+     */
+    public static void introducirDatoAlumno(Alumno[] alum) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("A continuacion debera introducir el nombre y la nota media de 5 alumnos.");
 
-    String nombreIntroducido;
-    double notaIntroducida;
-    
-    for(int i = 0; i < 5; i++) {
-      
-      alum[i] = new Alumno();          
-      System.out.println("Alumno " + i);
-      System.out.print("Nombre: ");
-      nombreIntroducido = entrada.nextLine();
-      (alum[i]).nombre = nombreIntroducido;
-      System.out.print("Nota media: ");
-      notaIntroducida = Double.parseDouble(entrada.nextLine());
-      alum[i].notaMedia = notaIntroducida;
-    } // for i
-    
-    // Muestra los datos de los alumnos /////////////////////////////////
-    
-    System.out.println("Los datos introducidos son los siguientes:");
+        for (int i = 0; i < 5; i++) {
 
-    double  sumaDeMedias = 0;
-    
-    for(int i = 0; i < 5; i++) {
-      System.out.println("Alumno " + i);  
-      System.out.println("Nombre: " + alum[i].nombre);    
-      System.out.println("Nota media: " + alum[i].notaMedia);
-      System.out.println("----------------------------");
+            alum[i] = new Alumno();
 
-      sumaDeMedias += alum[i].notaMedia;
-    } // for i
-    
-    System.out.println("La media global de la clase es " + sumaDeMedias / 5);
-  }
+            System.out.println("Alumno " + i);
+            System.out.print("Nombre: ");
+            (alum[i]).setNombre(entrada.next());
+            System.out.print("Nota media: ");
+            (alum[i]).setNotaMedia((entrada.nextDouble()));
+        }
+    }
+
+    /**
+     * Se muestra la informacion del alumno y la nota media global
+     * @param alum Array donde se almacena los alumnos
+     */
+    public static void mostrarInformacion(Alumno[] alum) {
+        System.out.println("Los datos introducidos son los siguientes:");
+        double sumaDeMedias = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Alumno " + i);
+            System.out.println("Nombre: " + alum[i].getNombre());
+            System.out.println("Nota media: " + alum[i].getNotaMedia());
+            System.out.println("----------------------------");
+
+            sumaDeMedias += alum[i].getNotaMedia();
+        }
+
+        System.out.println("La media global de la clase es " + sumaDeMedias / 5);
+    }
 }
